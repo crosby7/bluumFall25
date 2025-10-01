502b0649d6094719927361d28adb60183d45607a
crosby7 <53411767+crosby7@users.noreply.github.com>
2025-10-01 15:34:31 -0400

    Created remaining Popups
    Popups for new task form and confirmation of new patients/tasks.
    
    Needs JS to control buttons and needs proper form submission

--- server-laravel/public/scripts/nurses.js
+++ server-laravel/public/scripts/nurses.js
@@ -5,62 +5,95 @@ const loginButton = document.querySelector(".loginButton");
 const registerButton = document.querySelector(".registerButton");
 const loginButtonDiv = document.querySelector(".loginButtons");
 
-
 if (toggleBtn) {
-  toggleBtn.addEventListener("click", () => {
-    sidebar.classList.toggle("open");
-  });
+    toggleBtn.addEventListener("click", () => {
+        sidebar.classList.toggle("open");
+    });
 }
 
 // Large Cards: close button functionality
 const closeButtons = document.querySelectorAll(".closeButton");
 
 if (closeButtons) {
-  closeButtons.forEach((button) => {
-    button.addEventListener("click", () => {
-      console.log("close button clicked");
-      button.closest(".popUp").classList.toggle("close");
-      if (button.closest(".popUp").parentElement.classList.contains("popUpRegion")) {
-        button.closest(".popUpRegion").classList.toggle("close");
-      }
-      if (loginButtonDiv) {
-        loginButtonDiv.classList.remove("close");
-      }
+    closeButtons.forEach((button) => {
+        button.addEventListener("click", () => {
+            console.log("close button clicked");
+            button.closest(".popUp").classList.toggle("close");
+            if (
+                button
+                    .closest(".popUp")
+                    .parentElement.classList.contains("popUpRegion")
+            ) {
+                button.closest(".popUpRegion").classList.toggle("close");
+            }
+            if (loginButtonDiv) {
+                loginButtonDiv.classList.remove("close");
+            }
+        });
     });
-  });
 }
 
 // Temp: submit button will just close the large card
 const submitButtons = document.querySelectorAll(".submitButton");
 
 if (submitButtons) {
-  submitButtons.forEach((button) => {
-  button.addEventListener("click", () => {
-    console.log("submit button clicked");
-    button.closest(".popUp").classList.toggle("close");
-    if (button.closest(".popUp").parentElement.classList.contains("popUpRegion")) {
-      button.closest(".popUpRegion").classList.toggle("close");
-    }
-    if (loginButtonDiv) {
-      loginButtonDiv.classList.remove("close");
-    }
-  });
-});
+    submitButtons.forEach((button) => {
+        button.addEventListener("click", () => {
+            console.log("submit button clicked");
+            button.closest(".popUp").classList.toggle("close");
+            if (
+                button
+                    .closest(".popUp")
+                    .parentElement.classList.contains("popUpRegion")
+            ) {
+                button.closest(".popUpRegion").classList.toggle("close");
+            }
+            if (loginButtonDiv) {
+                loginButtonDiv.classList.remove("close");
+            }
+        });
+    });
 }
 
 // login and register buttons will open popup card
 if (loginButton) {
-  loginButton.addEventListener("click", () => {
-  console.log("login button clicked");
-  loginButtonDiv.classList.toggle("close");
-  document.querySelector(".login").classList.toggle("close");
-});
-
+    loginButton.addEventListener("click", () => {
+        console.log("login button clicked");
+        loginButtonDiv.classList.toggle("close");
+        document.querySelector(".login").classList.toggle("close");
+    });
 }
 if (registerButton) {
-  registerButton.addEventListener("click", () => {
-    console.log("register button clicked");
-    loginButtonDiv.classList.toggle("close");
-    document.querySelector(".register").classList.toggle("close");
-  });
+    registerButton.addEventListener("click", () => {
+        console.log("register button clicked");
+        loginButtonDiv.classList.toggle("close");
+        document.querySelector(".register").classList.toggle("close");
+    });
+}
+
+// New Patient Button will open New Patient Pop Up
+const newPatientButtons = document.querySelectorAll(".addNewPatient");
+const newPatientPopUp = document.querySelector(".newPatient");
+const confirmPatientPopUp = document.querySelector(".confirmPatient");
+
+if (newPatientButtons) {
+    newPatientButtons.forEach((button) => {
+        button.addEventListener("click", () => {
+            console.log("new patient button clicked");
+            // If the confirmation pop up is open, close it
+            if (
+                confirmPatientPopUp &&
+                !confirmPatientPopUp.classList.contains("close")
+            ) {
+                confirmPatientPopUp.classList.add("close");
+            }
+            // Open the new patient pop up
+            newPatientPopUp.classList.remove("close");
+
+            // Open the pop up region if it's closed
+            if (newPatientPopUp.parentElement.classList.contains("close")) {
+                newPatientPopUp.parentElement.classList.remove("close");
+            }
+        });
+    });
 }
--- server-laravel/resources/views/screens/patients.blade.php
+++ server-laravel/resources/views/screens/patients.blade.php
@@ -42,8 +42,8 @@
     </div>
 
     {{-- Pop Up for New Patient Initialization --}}
-    <div class="popUpRegion">
-        <div class="largeCard popUp">
+    <div class="popUpRegion close">
+        <div class="largeCard popUp close newPatient">
             <button class="closeButton xButton" type="button">X</button>
             <form class="newPatientForm">
                 <div class="formInputs">
@@ -75,16 +75,94 @@
                             <option value="3909">Room 3909</option>
                         </select>
                     </div>
-                    <div class="inputGroup">
-                        <label for="patientNotes">Notes</label>
-                        <textarea id="patientNotes" name="patientNotes" placeholder="Anything to add about this patient..." required></textarea>
-                    </div>
                 </div>
                 <div class="submitButtons">
-                    <button type="submit" class="submitButton">Create Patient</button>
+                    <button type="submit" class="submitButton createPatient">Create Patient</button>
                     <button type="button" class="cancelButton closeButton">Cancel</button>
                 </div>
             </form>
+        </div>
+        <div class="popUp largeCard confirmPatient close">
+            <button class="closeButton xButton" type="button">X</button>
+            <div class="confirmation">
+                <img src="{{ asset('assets/tasks/statusComplete.svg') }}" alt="">
+                <div class="confirmationText">
+                    <h3>New Patient Registered!</h3>
+                    <p>You've successfully registered a new patient.</p>
+                </div>
+                <button type="button" class="addNewPatient">New Patient</button>
+                <button type="button" class="closeButton maybeLater">Maybe Later</button>
+            </div>
+        </div>
+        <div class="newTask largeCard popUp close">
+            <button class="closeButton xButton" type="button">X</button>
+            <form class="newTaskForm">
+                <div class="formInputs">
+                    <select name="taskName" id="taskName" class="fullWidthSelect">
+                        <option value="" disabled selected>Select task...</option>
+                        <option value="medication">Morning Medication</option>
+                        <option value="grooming">Grooming</option>
+                        <option value="ptTherapy">PT Therapy</option>
+                        <option value="breakfast">Breakfast</option>
+                        <option value="lunch">Lunch</option>
+                        <option value="dinner">Dinner</option>
+                        <option value="otTherapy">OT Therapy</option>
+                        <option value="recActivity">Recreation Activity</option>
+                        <option value="reading">School: Reading</option>
+                        <option value="math">School: Math</option>
+                        <option value="bandages">Bandage Change</option>
+                    </select>
+                    <div class="inputGroup">
+                        <label for="taskAssignee">Assignee</label>
+                        <select name="taskAssignee" id="taskAssignee">
+                            <option value="" disabled selected>No assignee</option>
+                            <option value="patient1">dev</option>
+                            <option value="patient2">Jared Carthalion</option>
+                            <option value="patient3">Jace Beleren</option>
+                        </select>
+                    </div>
+                    <div class="inputGroup">
+                        <label for="dueDate">Due Date & Time</label>
+                        <input type="datetime-local" id="dueDate" name="dueDate" required>
+                    </div>
+                    <div class="twoInputs">
+                        <div class="inputGroup">
+                            <label for="repeats">Repeats</label>
+                            <select name="repeats" id="repeats">
+                                <option value="none" selected>No repeats</option>
+                                <option value="daily">Daily</option>
+                                <option value="weekly">Weekly</option>
+                                <option value="monthly">Monthly</option>
+                            </select>
+                        </div>
+                        <div class="inputGroup">
+                            <label for="category">Category</label>
+                            <select name="category" id="category">
+                                <option value="personal">Self Care</option>
+                                <option value="medical">Medical</option>
+                                <option value="nutritional">Nutritional</option>
+                                <option value="school">School</option>
+                                <option value="recreational">Recreational</option>
+                            </select>
+                        </div>
+                    </div>
+                </div>
+                    <div class="submitButtons">
+                        <button type="submit" class="submitButton createTask">Create Task</button>
+                        <button type="button" class="cancelButton closeButton">Cancel</button>
+                    </div>
+        </div>
+        <div class="popUp largeCard confirmTask close">
+            <button class="closeButton xButton" type="button">X</button>
+            <div class="confirmation">
+                <img src="{{ asset('assets/tasks/statusComplete.svg') }}" alt="">
+                <div class="confirmationText">
+                    <h3>Task Created!</h3>
+                    <p>You've successfully assigned a new task.</p>
+                </div>
+                <button type="button" class="addNewTask">New Task</button>
+                <button type="button" class="closeButton maybeLater">Maybe Later</button>
+            </div>
         </div>
     </div>
 @endsection
