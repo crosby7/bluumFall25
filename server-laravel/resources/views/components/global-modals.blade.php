6c4bbc8c1ed38898ee2b617eccca6c0a44710210
crosby7 <53411767+crosby7@users.noreply.github.com>
2025-11-16 10:28:53 -0500

    Fix missing closing tag on newTaskForm
    See Issues #7

--- server-laravel/resources/views/components/global-modals.blade.php
+++ server-laravel/resources/views/components/global-modals.blade.php
@@ -124,10 +124,11 @@
                     </div>
                 </div>
             </div>
-                <div class="submitButtons">
-                    <button type="submit" class="submitButton createTask">Create Task</button>
-                    <button type="button" class="cancelButton closeButton">Cancel</button>
-                </div>
+            <div class="submitButtons">
+                <button type="submit" class="submitButton createTask">Create Task</button>
+                <button type="button" class="cancelButton closeButton">Cancel</button>
+            </div>
+        </form>
     </div>
 
     {{-- New Task Confirmation Modal --}}
